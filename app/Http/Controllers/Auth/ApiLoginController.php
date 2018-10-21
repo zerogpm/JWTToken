487e42b0562822eed83db27caf699be79d63947a
zerogpm <zerogpm@gmail.com>
2018-10-20 20:33:59 -0700

    add user resources

---
+++ app/Http/Controllers/Auth/ApiLoginController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Resources\UserResource;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ApiLoginController extends Controller
+{
+    public function login(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+
+        if (!$token = auth()->attempt($request->only(['email', 'password']))) {
+            return response()->json([
+                'errors' => [
+                    'email' => ['Sorry We couldn\'sign you in']
+                ]
+            ], 422);
+        }
+
+        return (new UserResource($request->user()))
+            ->additional([
+                'meta' => [
+                    'token' => $token
+                ]
+            ]);
+    }
+}
